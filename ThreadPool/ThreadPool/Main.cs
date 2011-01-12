765557e91e6a069b4118d093d0589f10a29792f7
Tomasz Wsuł <2nickers@gmail.com>
2011-01-12 12:25:35 +0100

    cleaning main

--- ThreadPool/ThreadPool/Main.cs
+++ ThreadPool/ThreadPool/Main.cs
@@ -3,6 +3,7 @@ using ThreadsIfaces;
 using System.Threading;
 using Mono.Unix.Native;
 
+
 namespace nThreadPool
 {
 	class MainClass
@@ -13,12 +14,13 @@ namespace nThreadPool
 		public static Object sync = new Object();
 		public static Int64 cnt = 0;
 		
-		public static void ddd()
+		public static void SuperTest()
 		{
 			long tm=0;
 			Mono.Unix.Native.Syscall.time(out tm);
 			nThreadPool tp = new nThreadPool(3);
-			for (int i=0; i<TEST_SIZE; i++) {
+			for (int i=0; i<TEST_SIZE; i++) 
+			{
 				tp.QueueUserWorkItem((Object o) => {
 					Monitor.Enter(MainClass.sync);
 					MainClass.cnt++;
@@ -39,8 +41,9 @@ namespace nThreadPool
 					Monitor.Exit(MainClass.sync);
 				});
 			}
-			
-			/*
+			Console.WriteLine("dodano wszystko");
+
+/*
 			Thread.Sleep(5);
 			tp.SetPoolSize(10);
 			Thread.Sleep(11);
@@ -51,120 +54,19 @@ namespace nThreadPool
 			tp.SetPoolSize(30);
 			tp.SetPoolSize(3);
 			tp.SetPoolSize(1);
-			*/
-//			Thread.Sleep(30);
-			
+*/
 			while (MainClass.cnt<TEST_SIZE) {
-				Thread.Sleep(100);
+				Thread.Sleep(300);
 				Console.WriteLine(MainClass.cnt);
 			}
 			
-			
 			Console.WriteLine("koniec");
 		}
-		
-		/**
-		 * 
-		 */
-		public static void ddd2()
-		{
-			int i1,i2;
-			MainClass.cnt=0;
-			long tm=0;
-			Mono.Unix.Native.Syscall.time(out tm);
-//			nThreadPool tp = new nThreadPool(3);
-			ThreadPool.GetMaxThreads(out i1, out i2);
-			ThreadPool.SetMaxThreads(3,i2);
-			ThreadPool.SetMinThreads(3,i2);
-			                         
-			for (int i=0; i<TEST_SIZE; i++) {
-				ThreadPool.QueueUserWorkItem((Object o) => {
-					Monitor.Enter(MainClass.sync);
-					MainClass.cnt++;
-					if (MainClass.cnt%MOD==0)
-					{
-						//IThreadPool p = (IThreadPool)o;
-						//long i1,i2;
-						ThreadPool.GetMaxThreads(out i1,out i2);
-						if (i1==10)
-						{
-							ThreadPool.SetMaxThreads(100,i2);
-							ThreadPool.SetMinThreads(100,i2);
-						}
-						else
-						{
-							ThreadPool.SetMaxThreads(10,i2);
-							ThreadPool.SetMinThreads(10,i2);
-						}
-					}
-					if (MainClass.cnt>=TEST_SIZE)
-					{
-						long tm2=0;
-						Mono.Unix.Native.Syscall.time(out tm2);
-						Console.WriteLine("Time: " + (tm2-tm));
-					}
-					Monitor.Exit(MainClass.sync);
-				});
-			}
-			
-			/*
-			Thread.Sleep(5);
-			ThreadPool.SetMaxThreads(10,i2);
-			ThreadPool.SetMinThreads(10,i2);
-			Thread.Sleep(11);
-			ThreadPool.SetMaxThreads(2,i2);
-			ThreadPool.SetMinThreads(2,i2);
-			Thread.Sleep(11);
-			ThreadPool.SetMaxThreads(20,i2);
-			ThreadPool.SetMinThreads(20,i2);
-			
-			ThreadPool.SetMaxThreads(12,i2);
-			ThreadPool.SetMinThreads(12,i2);
-			
-			ThreadPool.SetMaxThreads(30,i2);
-			ThreadPool.SetMinThreads(30,i2);
-			
-			ThreadPool.SetMaxThreads(3,i2);
-			ThreadPool.SetMinThreads(3,i2);
-			
-			ThreadPool.SetMaxThreads(1,i2);
-			ThreadPool.SetMinThreads(1,i2);
-			*/
-			
-//			Thread.Sleep(30);
-			
-			while (MainClass.cnt<TEST_SIZE) {
-				Thread.Sleep(100);
-				Console.WriteLine(MainClass.cnt);
-			}
-			
-			
-			Console.WriteLine("koniec");
-		}
-		
+
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
-			
-			for (int i=0; i<args.Length; i++) 
-			{
-				Console.WriteLine(i + ") " + args[i]);
-			}
-			
-			//if (args.Length>0 && args[0]=="my")
-			{
-				MainClass.ddd();
-				Console.WriteLine("Main finish");
-			}
-//			Thread.Sleep(3000);
-
-			//if (args.Length==0 || args[0]=="tp") 
-			{
-			//	MainClass.ddd2();
-			}
-//			Console.WriteLine("Main finish");
-//			Thread.Sleep(3000);
-			
+			Console.WriteLine (" >> Hello World! << ");
+			MainClass.SuperTest();
 			Console.WriteLine(" >> koniec << ");
 		}
 	}
